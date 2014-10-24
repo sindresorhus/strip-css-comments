3a5ce778272dcf7b7b09b723c201a7679314394e
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-24 14:05:40 +0200

    add benchmark - fixes #2

---
+++ bench.js
@@ -0,0 +1,8 @@
+'use strict';
+var fs = require('fs');
+var stripCssComments = require('./');
+var fixture = fs.readFileSync('fixture.css', 'utf8');
+
+bench('strip CSS comments', function () {
+	stripCssComments(fixture);
+});
