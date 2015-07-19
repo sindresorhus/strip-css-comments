2d3ebfac8e15d484963deeb32587d5ee8d5169c5
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-07-19 06:07:53 +0500

    Don't create new string to find /* and */
    Before:
                 74 op/s » strip CSS comments
    After:
                 155 op/s » strip CSS comments

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ module.exports = function (str, opts) {
 		}
 
 		// start /* type comment
-		if (!insideString && currentChar + str[i + 1] === '/*') {
+		if (!insideString && currentChar === '/' && str[i + 1] === '*') {
 			if (!preserve || preserve && str[i + 2] !== '!') {
 				// start skipping until we reach end of comment
 				for (var j = i + 2; j < str.length; j++) {
-					if (str[j] + str[j + 1] === '*/') {
+					if (str[j] === '*' && str[j + 1] === '/') {
 						break;
 					}
 				}
